181f4081f96749efb5ea3596c0f59e40f813ecad
Honggang (Ryan) Zhao <ryan890815@gmail.com>
2018-06-13 22:23:56 -0500

    Update mkdir.sh

--- mkdir.sh
+++ mkdir.sh
@@ -3,10 +3,10 @@ do
 	for j in {1..10}
 	do 	
 		mkdir k'_'$i'_'$j
-		cp /mnt/homeapps/home/aubhyz/structure/WB166_SNP426_structure_input k'_'$i'_'$j/
-		cp /mnt/homeapps/home/aubhyz/structure/mainparams k'_'$i'_'$j/
-		cp /mnt/homeapps/home/aubhyz/structure/structure k'_'$i'_'$j/
-		cp /mnt/homeapps/home/aubhyz/structure/extraparams k'_'$i'_'$j/
+		cp ../structure_input k'_'$i'_'$j/
+		cp ../mainparams k'_'$i'_'$j/
+		cp ../structure k'_'$i'_'$j/
+		cp ../extraparams k'_'$i'_'$j/
 		sed -i '15s/.*/#define MAXPOPS    '$i'      \/\/ (int) number of populations assumed/' k'_'$i'_'$j/mainparams
 		sed -i '22s/.*/#define OUTFILE  WB166_SNP426_structure_output_K'$i'_run_'$j' \/\/(str) name of output data file/' k'_'$i'_'$j/mainparams
 		echo './structure' > k'_'$i'_'$j/run.sh
